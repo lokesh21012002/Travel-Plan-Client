e502e683ea933cad4b4c6eb8fd0febbff407f0f8
lokesh21012002 <83236804+lokesh21012002@users.noreply.github.com>
2023-11-28 14:11:44 +0530

    Update AxiosConfig.js

--- src/config/AxiosConfig.js
+++ src/config/AxiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://51.20.128.201/api/v1/",
+  // baseURL: "http://51.20.128.201/api/v1/",
+    baseURL: "http://15.206.163.196:8080/api/v1/",
   
 });
